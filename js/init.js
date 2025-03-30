70da17f64507f5d159667c13941b4c1482827b1e
juining119 <71023631+juining119@users.noreply.github.com>
2025-03-31 01:21:34 +0800

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,28 @@
+﻿window.onload = init;
+
+function init() {
+    $("#gun_left").jsMovie({
+        sequence: 'gunLeft_0#.svg'
+        , from: 1
+        , to: 6
+        , fps: 10
+        , repeate: true
+        , folder: "images/gun/"
+        , showPreLoader: false
+        , width: 100 + "%"
+        , height: 100
+    });
+    $("#gun_right").jsMovie({
+        sequence: 'gunRight_0#.svg'
+        , from: 1
+        , to: 6
+        , fps: 10
+        , repeate: true
+        , folder: "images/gun/"
+        , showPreLoader: false
+        , width: 100 + "%"
+        , height: 100
+    });
+    $("#gun_left").jsMovie("play");
+    $("#gun_right").jsMovie("play");
+}
